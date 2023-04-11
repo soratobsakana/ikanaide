fec6821feeaf6d58b8d7475323f8c31f75228427
nabuna <nabuna@proton.me>
2023-04-11 09:14:13 +0200

    Fix rankings giving error if  is null.

--- resources/views/rankings/_ranking.view.php
+++ resources/views/rankings/_ranking.view.php
@@ -2,22 +2,26 @@
     <section class="rankings-page_anime_content">
         <?php
         
-        for($i=0; $i<count($ranking); $i++) {
-            ?>
-
-            <div class="rankings-page_anime_content-entry">
-                <div class="center-text"><h3>#<?=$ranking[$i]['rank']?></h3></div>
-                <div><a href="/<?=$medium?>/<?=str_replace(' ', '-', $ranking[$i]['title'])?>"><img src="<?=$ranking[$i]['cover']?>" alt=""></a></div>
-                <div class="low-opacity"><a href="/<?=$medium?>/<?=str_replace(' ', '-', $ranking[$i]['title'])?>"><?=$ranking[$i]['title']?></a></div>
-                <div class="center-text"><?=$ranking[$i]['score']?></div>
-                <div class="center-text"><?=$ranking[$i]['members']?></div>
-                <div class="list">
+        if (!is_null($ranking)) {
+            for($i=0; $i<count($ranking); $i++) {
+                ?>
+    
+                <div class="rankings-page_anime_content-entry">
+                    <div class="center-text"><h3>#<?=$ranking[$i]['rank']?></h3></div>
+                    <div><a href="/<?=$medium?>/<?=str_replace(' ', '-', $ranking[$i]['title'])?>"><img src="<?=$ranking[$i]['cover']?>" alt=""></a></div>
+                    <div class="low-opacity"><a href="/<?=$medium?>/<?=str_replace(' ', '-', $ranking[$i]['title'])?>"><?=$ranking[$i]['title']?></a></div>
+                    <div class="center-text"><?=$ranking[$i]['score']?></div>
+                    <div class="center-text"><?=$ranking[$i]['members']?></div>
+                    <div class="list">
+                    </div>
                 </div>
-            </div>
-        
-            <?php
+            
+                <?php
+            }
+        } else {
+            print '<i class="center-text">This seems empty...</i>';
         }
-        
+
         ?>
         
     </section>
